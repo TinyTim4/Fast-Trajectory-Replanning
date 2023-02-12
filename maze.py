c3faa6c16e12b4f66654a859ba530961ccae22db
Dylan-Mohr <73261726+Dylan-Mohr@users.noreply.github.com>
2023-02-12 10:45:22 -0500

    Update maze.py
    Added obstacle boolean to Node class and have each node print out there Node.obs value before the table.

--- maze.py
+++ maze.py
@@ -2,12 +2,13 @@ import random
 from heapq import heapify, heappush, heappop
 
 class Node:
-    def __init__(self, gval, xcoord, ycoord, parent, hval):
+    def __init__(self, gval, xcoord, ycoord, obs, parent, hval):
         self.gval = gval
         self.hval = hval
         self.fval = gval + hval
         self.xcoord = xcoord
         self.ycoord = ycoord
+        self.obs = False
         self.parent = parent
 
 def make_maze(): 
@@ -17,25 +18,33 @@ def make_maze():
         sampleList, weights=(70,30), k=10) for j in range(rows)]
     arr[0][0] = '0'
     arr[9][9] = '0'
+    for i in range(cols):
+        for j in range(rows):
+            if arr[i][j] == '1':
+                Node.obs = True
+                print(Node.obs)
+            else:
+                Node.obs = False
+                print(Node.obs)
     return arr
 
 def astar(robx, roby, goalx, goaly):
     openList = []
     closedList = {}
     heapify(openList)
-    current = Node(0,robx,roby,None, calculateDistance(robx,roby,goalx,goaly))
+    current = Node(0,robx,roby, False, None, calculateDistance(robx,roby,goalx,goaly))
     while(True):
         if(current.ycoord > 0):
-            north = Node(current.gval+1,current.xcoord,current.ycoord-1,current, calculateDistance(current.xcoord,current.ycoord-1,goalx,goaly))
+            north = Node(current.gval+1,current.xcoord,current.ycoord-1, False, current, calculateDistance(current.xcoord,current.ycoord-1,goalx,goaly))
             heappush(openList, (north.fval,north.hval,1, north))
         if(current.ycoord < goaly):
-            south = Node(current.gval+1,current.xcoord,current.ycoord+1,current, calculateDistance(current.xcoord,current.ycoord+1,goalx,goaly))
+            south = Node(current.gval+1,current.xcoord,current.ycoord+1,False, current, calculateDistance(current.xcoord,current.ycoord+1,goalx,goaly))
             heappush(openList, (south.fval, south.hval, 2, south))
         if(current.xcoord < goalx):
-            east = Node(current.gval+1,current.xcoord+1,current.ycoord,current, calculateDistance(current.xcoord+1,current.ycoord,goalx,goaly))
+            east = Node(current.gval+1,current.xcoord+1,current.ycoord, False, current, calculateDistance(current.xcoord+1,current.ycoord,goalx,goaly))
             heappush(openList, (east.fval, east.hval,3,  east))
         if(current.xcoord > 0):
-            west = Node(current.gval+1,current.xcoord-1,current.ycoord,current, calculateDistance(current.xcoord-1,current.ycoord,goalx,goaly))
+            west = Node(current.gval+1,current.xcoord-1,current.ycoord, False, current, calculateDistance(current.xcoord-1,current.ycoord,goalx,goaly))
             heappush(openList, (west.fval, west.hval,4, west))
         tempNode = heappop(openList)[3]
         if(tempNode.hval == 0):
